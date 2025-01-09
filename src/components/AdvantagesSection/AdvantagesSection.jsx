a96a708961e9caf724ccb8655c0c870f83fe888b
Viktor S <sandorikviktor@gmail.com>
2025-01-09 18:28:18 +0200

    del us

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -1,15 +1,10 @@
 import css from "./AdvantagesSection.module.css";
-import UserCount from "./UserCount.jsx";
 
 const AdvantagesSection = () => {
   
   return (
     <div className={css.advantagesSection}>
-      <div className={css.userCount}>
-        <UserCount />
-      </div>
       
-
       <ul className={css.advantagesList}>
         <li className={css.advantagesHabit}>Habit drive</li>
         <li className={css.advantagesStatistics}>View Statistics</li>
