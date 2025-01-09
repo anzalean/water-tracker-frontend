8c939e0519567fce24e735f319f6f1f691303794
gocha001 <kalianichgocha@ukr.net>
2025-01-09 13:20:40 +0200

    add mod

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,15 +2,28 @@ import { Helmet } from 'react-helmet-async';
 import { Container } from '../../components/Container/Container';
 import { Page } from '../../components/Page/Page.jsx';
 import css from './HomePage.module.css'; 
+import { useState } from 'react';
+import { Modal } from '../../components/Modal/Modal.jsx';
 
 export default function HomePage() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
   return (
     <Container>
       <Helmet>
         <title>Home</title>
       </Helmet>
       <Page>
-        {<div className={css.HomePage}>Home</div>}
+        {
+          <div className={css.HomePage}>
+            Home
+            <button onClick={openModal}></button>
+            {isOpen && (<Modal onClose={closeModal}>MODAL</Modal>)}
+          </div>
+        }
       </Page>
     </Container>
   );
