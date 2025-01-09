b28f180d0f6158999b529ed5f2637e1455558ce1
cptSmollet <150056647+cptSmollet@users.noreply.github.com>
2025-01-09 16:38:15 +0100

    update addwaterbutton and fix styles

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -2,9 +2,14 @@ import { Helmet } from 'react-helmet-async';
 import { Container } from '../../components/Container/Container';
 import { Page } from '../../components/Page/Page';
 import WaterProgressBar from './WaterProgressBar/WaterProgressBar';
+import AddWaterButton from './AddWaterButton/AddWaterButton';
 import css from './TrackerPage.module.css';
 
 export default function TrackerPage() {
+  const handleAddWater = () => {
+    console.log('Add Water button clicked'); // В дальнейшем здесь будет логика для открытия модального окна
+  };
+
   return (
     <Container>
       <Helmet>
@@ -14,6 +19,7 @@ export default function TrackerPage() {
         <div className={css.TrackerPage}>
           <h1>Water Tracker</h1>
           <WaterProgressBar progress={50} />
+          <AddWaterButton onClick={handleAddWater} />
         </div>
       </Page>
     </Container>
