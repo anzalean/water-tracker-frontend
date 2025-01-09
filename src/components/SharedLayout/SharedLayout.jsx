e032ade788132fe80d2b6c151553a34f3db015c0
Galina Mykolaychuk (Flawia) <gflawiae@gmail.com>
2025-01-09 20:14:58 +0200

    fix bugs with Logo component

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,10 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import Logo from "../Logo/Logo";
-import css from "./SharedLayout.module.css";
 
 export const SharedLayout = () => {
   return (
     <div>
       <Suspense fallback={null}>
-        <header className={css.header}>
-          <Logo />
-        </header>
         <Outlet />
       </Suspense>
     </div>
