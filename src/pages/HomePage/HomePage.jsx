b47125ff1f8271a756e83cbc2ea414f000a06306
Galina Mykolaychuk (Flawia) <gflawiae@gmail.com>
2025-01-11 00:22:43 +0200

    add WelcomeSection component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,8 @@
-import { Helmet } from 'react-helmet-async';
-import { Container } from '../../components/Container/Container';
-import { Page } from '../../components/Page/Page.jsx';
-import css from './HomePage.module.css'; 
+import { Helmet } from "react-helmet-async";
+import { Container } from "../../components/Container/Container";
+import { Page } from "../../components/Page/Page.jsx";
+import { WelcomeSection } from "../../components/WelcomeSection/WelcomeSection.jsx";
+import css from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
@@ -10,7 +11,9 @@ export default function HomePage() {
         <title>Home</title>
       </Helmet>
       <Page>
-        {<div className={css.HomePage}>Home</div>}
+        <div className={css.HomePage}>
+          <WelcomeSection />
+        </div>
       </Page>
     </Container>
   );
