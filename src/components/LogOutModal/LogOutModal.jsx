e057e78812b39394b4700b24df770b6d540c7c75
IrynaKryvenko <kryvenkoirina93@gmail.com>
2025-01-10 18:11:07 +0200

    upload

---
+++ src/components/LogOutModal/LogOutModal.jsx
@@ -0,0 +1,39 @@
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../redux/selectors";
+import Modal from "../Modal/Modal";
+import css from "./LogOutModal.module.css";
+
+const LogOutModal = ({ onClose }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    try {
+      await dispatch(logout());
+      navigate("/");
+      onClose();
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
+  };
+
+  return (
+    <Modal onClose={onClose}>
+      <div>
+        <h2 className={css.logOutTitle}>Log out</h2>
+        <p className={css.logOutCaption}>Do you really want to leave?</p>
+        <div className={css.logOutBtnCont}>
+          <button className={css.logButton} onClick={handleLogOut}>
+            Log out
+          </button>
+          <button className={css.cancelButton} onClick={onClose}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default LogOutModal;
