80ca5d54d44885905507b88830c2fb9b5ff019de
anzalean <zablovskaya04@gmail.com>
2025-01-13 15:06:35 +0200

    fixed

--- src/components/AddWaterButton/AddWaterButton.jsx
+++ src/components/AddWaterButton/AddWaterButton.jsx
@@ -3,8 +3,8 @@ import styles from './AddWaterButton.module.css';
 const AddWaterButton = ({ onClick }) => {
   return (
     <button className={styles.buttonContainer} onClick={onClick}>
-      <svg className={styles.buttonIcon} width="16" height="16" fill="var(--color-white)">
-        <use href="/src/assets/icons/sprite.svg#icon-plus-light"></use>
+      <svg className={styles.buttonIcon} width="16" height="16">
+        <use href="/src/assets/icons/sprite.svg#icon-plus"></use>
       </svg>
       <span className={styles.buttonText}>Add water</span>
     </button>
