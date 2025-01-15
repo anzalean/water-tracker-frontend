4cc461a74b8eb356aa22f3cbdd7107981c25def2
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-01-15 21:28:00 +0200

    PaginationCalendar

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,10 +1,15 @@
 {/* import DailyInfo from "../DailyInfo/DailyInfo"; */}
+import MonthInfo from "../MonthInfo/MonthInfo"; 
+import UserPanel from "../UserPanel/UserPanel";
 import styles from "./WaterDetailedInfo.module.css";
 
 const WaterDetailedInfo = () => {
   return (
     <section className={styles.waterDetailedInfo}>
       {/* <DailyInfo /> */}
+      <UserPanel/>
+      <MonthInfo/>
+
     </section>
   );
 };
