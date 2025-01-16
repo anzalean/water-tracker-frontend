e662f1f3bb50bb4c1ec76a76c0c92f817c3122b1
gocha001 <kalianichgocha@ukr.net>
2025-01-16 21:42:30 +0200

    Зберігаю локальні зміни перед злиттям

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,14 +1,14 @@
 import DailyInfo from "../DailyInfo/DailyInfo";
-import MonthInfo from "../MonthInfo/MonthInfo"; 
+import MonthInfo from "../MonthInfo/MonthInfo";
 import UserPanel from "../UserPanel/UserPanel";
 import styles from "./WaterDetailedInfo.module.css";
 
 const WaterDetailedInfo = () => {
   return (
     <section className={styles.waterDetailedInfo}>
-      <UserPanel/>
-     <DailyInfo /> 
-      <MonthInfo/>
+      <UserPanel />
+      <DailyInfo />
+      <MonthInfo />
     </section>
   );
 };
