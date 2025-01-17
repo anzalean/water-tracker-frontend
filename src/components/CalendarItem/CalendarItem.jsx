659373f5aedf3fb0c51ac6b0df33994f538eca9d
gocha001 <kalianichgocha@ukr.net>
2025-01-17 02:50:49 +0200

    save

--- src/components/CalendarItem/CalendarItem.jsx
+++ src/components/CalendarItem/CalendarItem.jsx
@@ -1,10 +1,22 @@
-import styles from './CalendarItem.module.css'; 
+import styles from "./CalendarItem.module.css";
 
-const CalendarItem = () => {
+const CalendarItem = ({ availability = 0, day, isActive }) => {
+  const buttonStyle = {
+    backgroundColor: isActive
+      ? "var(--color-darkblue)"
+      : availability < 100
+      ? "var(--color-darkblue-translucent)"
+      : "var(--color-white)",
+    color: isActive ? "var(--color-lightgreen)" : "var(--color-black)",
+  };
+
   return (
-    <li className={styles.itemCalendar}>
-      
-    </li>
+    <div className={styles.itemBox}>
+      <button className={styles.buttonDay} style={buttonStyle}>
+        {day}
+      </button>
+      <span className={styles.infoText}>{availability}%</span>
+    </div>
   );
 };
 
