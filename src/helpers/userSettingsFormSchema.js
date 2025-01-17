e23b8328b5162c8e64ebf46c1e13df192a998b78
aandrea-alex <151180549+aandrea-alex@users.noreply.github.com>
2025-01-17 20:18:15 +0100

    changed joi

--- src/helpers/userSettingsFormSchema.js
+++ src/helpers/userSettingsFormSchema.js
@@ -32,7 +32,7 @@ export const feedbackSchema = Yup.object().shape({
 
   desiredVolume: Yup.number()
     .typeError("Water intake must be a number")
-    .min(50, "Water intake must be at least 50 ml.")
-    .max(5000, "Water intake cannot exceed 5000 ml.")
+    .min(0.05, "Water intake must be at least 0.05 L.")
+    .max(5, "Water intake cannot exceed 5 mL.")
     .required("Water intake is required."),
 });
