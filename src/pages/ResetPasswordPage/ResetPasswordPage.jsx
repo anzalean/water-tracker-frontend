e3c3c12d34e483d8d5721928592799359a5a85ae
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-01-18 13:25:19 +0200

    ResetPasswordPage page + component

--- src/pages/ResetPasswordPage/ResetPasswordPage.jsx
+++ src/pages/ResetPasswordPage/ResetPasswordPage.jsx
@@ -2,6 +2,8 @@ import { Helmet } from 'react-helmet-async';
 import { Container } from '../../components/Container/Container';
 import { Page } from '../../components/Page/Page';
 import css from './ResetPasswordPage.module.css'; 
+import ResetPasswordForm from '../../components/ResetPasswordForm/ResetPasswordForm';
+import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
 
 export default function ResetPasswordPage() {
   return (
@@ -10,7 +12,12 @@ export default function ResetPasswordPage() {
         <title>Password Reset</title>
       </Helmet>
       <Page>
-        {<div className={css.ResetPasswordPage}>Password Reset</div>}
+        {<div className={css.ResetPasswordPage}>
+          <ResetPasswordForm/>
+          <div className={css.advantagesDesktopOnly}>
+              <AdvantagesSection />
+          </div>
+        </div>}
       </Page>
     </Container>
   );
