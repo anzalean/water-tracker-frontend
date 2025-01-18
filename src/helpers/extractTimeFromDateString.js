a5316b57886998e5ac0dc153103785555e14c567
aandrea-alex <151180549+aandrea-alex@users.noreply.github.com>
2025-01-18 21:09:28 +0100

    Update extractTimeFromDateString.js

--- src/helpers/extractTimeFromDateString.js
+++ src/helpers/extractTimeFromDateString.js
@@ -2,31 +2,21 @@ export function extractTimeFromDateString(dataString) {
   if (!dataString || typeof dataString !== "string") {
     throw new Error("Invalid date string");
   }
-  const date = new Date(dataString);
-  if (!isNaN(date.getTime())) {
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    return `${hours}:${minutes}`;
+
+  try {
+    const date = new Date(dataString);
+    if (!isNaN(date.getTime())) {
+      const hours = String(date.getUTCHours()).padStart(2, "0");
+      const minutes = String(date.getUTCMinutes()).padStart(2, "0");
+      if (hours > 23 || minutes > 59) {
+        throw new Error("Invalid time values");
+      }
+      return `${hours}:${minutes}`;
+    }
+  } catch (error) {
+    console.log(error.message);
+    throw new Error("Error processing the date string");
   }
 
-  const timeRegex = /^(\d{1,2}):(\d{2})(?::(\d{2}))?$/;
-  const match = dataString.match(timeRegex);
-
-  if (!match) {
-    throw new Error(
-      "String must contain a valid time in the format HH:mm or HH:mm:ss"
-    );
-  }
-
-  const hours = parseInt(match[1], 10);
-  const minutes = parseInt(match[2], 10);
-
-  if (hours > 23 || minutes > 59) {
-    throw new Error("Invalid time values");
-  }
-
-  return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
-    2,
-    "0"
-  )}`;
+  throw new Error("Invalid date string format");
 }
