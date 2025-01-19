5f36060973974ca0f85d0b5fc6306a0e677ce726
cptSmollet <150056647+cptSmollet@users.noreply.github.com>
2025-01-19 14:37:39 +0100

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,13 +1,13 @@
 import { Suspense } from "react";
 import { Toaster } from "react-hot-toast";
 import { Outlet } from "react-router-dom";
-import TempLoginBtn from "../../components/TempLogInBtn/TempLogInBtn";
+// import TempLoginBtn from "../../components/TempLogInBtn/TempLogInBtn";
 
 export const SharedLayout = () => {
   return (
     <div>
       <Suspense fallback={null}>
-        <TempLoginBtn />
+        {/* <TempLoginBtn /> */}
         <Outlet />
       </Suspense>
       <Toaster position="top-right" reverseOrder={false} />
