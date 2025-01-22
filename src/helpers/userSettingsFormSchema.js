c59a383e7a26d4cf720916521d8befd0f5d6e1bd
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-01-22 18:47:12 +0200

    Added Lang i18n

--- src/helpers/userSettingsFormSchema.js
+++ src/helpers/userSettingsFormSchema.js
@@ -1,30 +1,56 @@
 import * as Yup from "yup";
+import i18n from "i18next";
 
 export const feedbackSchema = Yup.object().shape({
   gender: Yup.string()
-    .oneOf(["male", "female"], "Gender must be either 'male' or 'female'")
+    .oneOf(
+      ["male", "female"],
+      i18n.t("validationSettings.gender.oneOf", {
+        defaultValue: "Gender must be either 'male' or 'female'",
+      })
+    )
     .default("female")
-    .typeError("Gender must be a string")
+    .typeError(
+      i18n.t("validationSettings.gender.typeError", {
+        defaultValue: "Gender must be a string",
+      })
+    )
     .notRequired(),
 
   name: Yup.string()
-    .min(2, "Name must be at least 2 characters long.")
-    .max(50, "Name cannot exceed 50 characters.")
-    .typeError("Name must be a string")
+    .min(
+      2,
+      i18n.t("validationSettings.name.min", {
+        defaultValue: "Name must be at least 2 characters long.",
+      })
+    )
+    .max(
+      50,
+      i18n.t("validationSettings.name.max", {
+        defaultValue: "Name cannot exceed 50 characters.",
+      })
+    )
+    .typeError(
+      i18n.t("validationSettings.name.typeError", {
+        defaultValue: "Name must be a string",
+      })
+    )
     .transform((value, originalValue) => {
       return originalValue === "" ? undefined : value;
     })
     .notRequired(),
 
   email: Yup.string()
-    .email("Please enter a valid email address.")
-    .typeError("Email must be a string")
-    .required("Email is required."),
+  .email(
+    i18n.t("validationSettings.email.email", { defaultValue: "Please enter a valid email address." })
+  )
+  .typeError(i18n.t("validationSettings.email.typeError", { defaultValue: "Email must be a string" }))
+  .required(i18n.t("validationSettings.email.required", { defaultValue: "Email is required." })),
 
   activityTime: Yup.number()
-    .typeError("Sport time must be a number")
-    .min(0, "Sport time cannot be negative.")
-    .max(24, "Sport time cannot exceed 24 hours.")
+  .typeError(i18n.t("validationSettings.activityTime.typeError", { defaultValue: "Sport time must be a number" }))
+  .min(0, i18n.t("validationSettings.activityTime.min", { defaultValue: "Sport time cannot be negative." }))
+  .max(24, i18n.t("validationSettings.activityTime.max", { defaultValue: "Sport time cannot exceed 24 hours." }))
     .default(0)
     .transform((value, originalValue) =>
       originalValue === "" || originalValue == null ? 0 : value
@@ -32,9 +58,9 @@ export const feedbackSchema = Yup.object().shape({
     .notRequired(),
 
   weight: Yup.number()
-    .typeError("Weight must be a number")
-    .min(0, "Weight cannot be negative.")
-    .max(350, "Weight cannot exceed 350 kg.")
+    .typeError(i18n.t("validationSettings.weight.typeError", { defaultValue: "Weight must be a number" }))
+    .min(0, i18n.t("validationSettings.weight.min", { defaultValue: "Weight cannot be negative." }))
+    .max(350, i18n.t("validationSettings.weight.max", { defaultValue: "Weight cannot exceed 350 kg." }))
     .default(0)
     .transform((value, originalValue) =>
       originalValue === "" || originalValue == null ? 0 : value
@@ -42,8 +68,11 @@ export const feedbackSchema = Yup.object().shape({
     .notRequired(),
 
   desiredVolume: Yup.number()
-    .typeError("Water intake must be a number")
-    .min(0.05, "Water intake must be at least 0.05 L.")
-    .max(5, "Water intake cannot exceed 5 L.")
-    .required("Water intake is required."),
+  .typeError(i18n.t("validationSettings.desiredVolume.typeError", { defaultValue: "Water intake must be a number" }))
+  .min(
+    0.05,
+    i18n.t("validationSettings.desiredVolume.min", { defaultValue: "Water intake must be at least 0.05 L." })
+  )
+  .max(5, i18n.t("validationSettings.desiredVolume.max", { defaultValue: "Water intake cannot exceed 5 L." }))
+  .required(i18n.t("validationSettings.desiredVolume.required", { defaultValue: "Water intake is required." })),
 });
